fa0266be54511059bb9ab1daf15411ab83f0a07e
Dheeraj-Manwani <dkmanwani2000@gmail.com>
2024-07-17 09:06:19 +0530

    docker compose

--- docker-roadmap-master/part-2/4-multi-stage-builds/usr/src/app/index.js
+++ docker-roadmap-master/part-2/4-multi-stage-builds/usr/src/app/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 // Set up mongoose connection
-mongoose.connect("mongodb://my_mongoss:27017/mydatabase", {});
+mongoose.connect("mongodb://localhost:27017/mydatabase", {});
 
 const EntrySchema = new mongoose.Schema({
   text: String,
@@ -16,13 +16,14 @@ const Entry = mongoose.model("Entry", EntrySchema);
 
 app.get("/", async (req, res) => {
   try {
-    const entry = new Entry({ text: "This is an entry by harkirat" });
+    const entry = new Entry({ text: "This is an entry" });
     await entry.save();
     res.send("Entry added!");
   } catch (err) {
     res.status(500).send("Error occurred");
   }
 });
+//
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
